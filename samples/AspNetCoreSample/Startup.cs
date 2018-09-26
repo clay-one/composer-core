5da15ac53775d1be6a9e28324a1b49bba6ba96e5
Hamed Iravanchi <iravanchi@gmail.com>
2018-09-26 17:25:48 +0330

    Add a simple ServiceProvider for Composer and initialize it inline in Startup

--- samples/AspNetCoreSample/Startup.cs
+++ samples/AspNetCoreSample/Startup.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ComposerCore.Cache;
+using ComposerCore.FluentExtensions;
+using ComposerCore.Implementation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -11,9 +11,33 @@ namespace AspNetCoreSample
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            var componentContext = new ComponentContext();
+            componentContext.Configuration.DisableAttributeChecking = true;
+            
+            foreach (var service in services)
+            {
+                if (service.ImplementationInstance != null)
+                {
+                    componentContext.ForObject(service.ImplementationInstance)
+                        .RegisterWith(service.ServiceType);
+                }
+                else if (service.ImplementationType != null)
+                {
+                    componentContext.ForComponent(service.ImplementationType)
+                        .UseComponentCache(GetComponentCacheType(service.Lifetime))
+                        .RegisterWith(service.ServiceType);
+                }
+                else if (service.ImplementationFactory != null)
+                {
+                    // TODO
+                }
+            }
+                        
+            return new ComposerServiceProvider(componentContext);
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -25,6 +49,24 @@ namespace AspNetCoreSample
 
             app.UseMvc();
             app.Run(async (context) => { await context.Response.WriteAsync("Hello World!"); });
+        }
+
+        private Type GetComponentCacheType(ServiceLifetime lifetime)
+        {
+            switch (lifetime)
+            {
+                case ServiceLifetime.Singleton:
+                    return typeof(ContractAgnosticComponentCache);
+                
+                case ServiceLifetime.Scoped:
+                    return null; // TODO
+                
+                case ServiceLifetime.Transient:
+                    return null;
+                
+                default:
+                    throw new InvalidOperationException($"Unsupported ServiceLifetime value {lifetime}");
+            }
         }
     }
 }
